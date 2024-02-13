90dcc287853ca38b6c6c10c3a2546e18f24b8024
YaroslawK <150711197+YaroslawK@users.noreply.github.com>
2024-02-13 12:22:25 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+function filterArray(numbers, value) {
+   const arr1 = [];
+    for (let i = 0; i < numbers.length; i++){
+        let a;
+        if (numbers[i] > value) {
+            arr1.push(numbers[i])
+        }
+    }
+    return arr1
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
