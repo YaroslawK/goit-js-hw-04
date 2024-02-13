9218e52a63d86146d462d81ae03dc4777c53130c
YaroslawK <xxjokerxx933@gmail.com>
2024-02-13 15:18:00 +0200

    hw4

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,22 @@
-function filterArray(numbers, value) {
-   const arr1 = [];
-    for (let i = 0; i < numbers.length; i++){
-        let a;
-        if (numbers[i] > value) {
-            arr1.push(numbers[i])
-        }
-    }
-    return arr1
-}
+const profile = {
+    username: "Jacob",
+    changeUsername(newName) {
+        this.username = newName;
+    },
+    playTime: 300,
+    updatePlayTime(hours) {
+        this.playTime += hours; 
+    }, 
+    getInfo() {
+      return `${this.username} has ${profile.playTime} active hours!`
+  }
+};
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
